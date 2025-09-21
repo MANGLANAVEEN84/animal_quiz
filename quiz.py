c3b5915b5195c819b549aa054882234d7dcbb98f
MANGLANAVEEN84 <MANGLANAVEEN84@GMAIL.COM>
2025-09-21 13:26:36 +0000

    Remove all leftover tkinter code after return in quiz.py for Streamlit compatibility

--- quiz.py
+++ quiz.py
@@ -9,13 +9,5 @@ def generate_quiz(num_questions: int = 5):
         question = f"Which one is {answer}?"
         quiz.append((question, options, answer))
     return quiz
-        for i in range(3):
-            btn = tk.Button(self.images_frame, command=lambda idx=i: self.on_choice(idx))
-            btn.grid(row=0, column=i, padx=10)
-            self.buttons.append(btn)
-
-        self.feedback_var = tk.StringVar()
-        self.feedback_label = tk.Label(self, textvariable=self.feedback_var, font=("Arial", 14))
-        self.feedback_label.pack(pady=6)
 
 
